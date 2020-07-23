5649f138e80e7272d5c868242fc6939b0ae3598e
fromjdobson <fromjdobson@gmail.com>
2020-07-23 11:23:16 -0400

    add footer

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default () => <footer style={{
+        textAlign: 'center',
+        margin: 'auto',
+        margin: 20,
+        color: '#242424'
+      }}>
+      
+        <span style={{
+            display: 'inline-block',
+            maxWidth: 600
+        }}>
+            <i>
+                A site to demonstrate and practice my "User Experience" and "design-to-code" abilities. Photos/icons from LittleCaesars.com
+            </i>
+        
+            <div style={{padding: 10}}>
+                <a href="https://fromjon.com"> Browse more apps at <u>  FromJon.com</u></a>
+            </div>
+        </span>
+      </footer>
+
